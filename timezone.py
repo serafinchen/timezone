a9ea0ee54d93cb4a87fd85af3a9ee672f09e7bc2
Serafina Schneider <serafina.schneider@uni-a.de>
2025-03-10 21:49:07 +0100

    fixes timezone class

--- timezone.py
+++ timezone.py
@@ -1,7 +1,6 @@
 from pytz import timezone
 from datetime import datetime, timezone
 import pytz
-import time
 
 class Timezone:
 
@@ -10,35 +9,32 @@ class Timezone:
         self.current_time = datetime.now()
         self.curren_timezone = self.current_time.astimezone().tzinfo
     
-        self.result_time = datetime.now(timezone.utc)
-        self.result_timezone = self.result_time.astimezone().tzinfo
+        self.result_time = datetime.now(pytz.utc)
+        self.result_timezone = self.result_time.tzinfo
         
 
     def get_current_time(self):
-        return str(self.current_time)
+        return self.current_time
     
     def get_current_timezone(self):
-        return str(self.curren_timezone)
+        return self.curren_timezone
     
     def get_result_time(self):
-        return str(self.result_time)
+        return self.result_time
     
     def get_result_timezone(self):
-        return str(self.result_timezone)
+        return self.result_timezone
     
     
     def change_result_timezone(self, result_timezone):
-        self.result_timezone = time.timezone(result_timezone)
-
-    def change_result_time(self, result_timezone):
-            self.result_time = self.current_time.astimezone(timezone(result_timezone))
-    
-    
+        self.result_time = datetime.now(pytz.timezone(result_timezone))
+        self.result_timezone = self.result_time.tzinfo
     
 if __name__ == '__main__':
     testTime = Timezone()
-    print(testTime.get_current_time())
-    print(testTime.get_current_timezone())
-    print()
+    print(testTime.get_result_time())
+    print(testTime.get_result_timezone())
+    print("-----")
+    testTime.change_result_timezone("Asia/Tokyo")
     print(testTime.get_result_time())
     print(testTime.get_result_timezone())

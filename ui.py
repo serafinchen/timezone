6f5ac9252cdc5a02425c5e30aae71515ae8632d8
serafinchen <serafina333@web.de>
2025-03-12 20:33:11 +0100

    implements timezone change

--- ui.py
+++ ui.py
@@ -15,10 +15,12 @@ class MainWindow(PySide6.QtWidgets.QMainWindow):
 
         #DropDown Box
         self.box = PySide6.QtWidgets.QComboBox()
-        for timezone in pytz.all_timezones:
-            self.box.addItem(timezone)
+        self.box.addItems(pytz.common_timezones)
 
         mainLayout.addWidget(self.box)
+
+
+        self.box.activated.connect(self.change_result_time)
 
         #Current Time and Current Timezone Lable
         current_time_widget = PySide6.QtWidgets.QWidget()
@@ -28,6 +30,7 @@ class MainWindow(PySide6.QtWidgets.QMainWindow):
         current_timezone = self.timezone.get_current_timezone()
         self.timezone_lable = PySide6.QtWidgets.QLabel(str(current_timezone))
         
+        
         current_timeLayout.addWidget(self.time_lable)
         current_timeLayout.addWidget(self.timezone_lable)
         mainLayout.addWidget(current_time_widget)
@@ -35,10 +38,10 @@ class MainWindow(PySide6.QtWidgets.QMainWindow):
         #Result Time and Result Timezone Lable
         result_time_widget = PySide6.QtWidgets.QWidget()
         result_time_widget.setLayout(result_timeLayout)
-        result_time = self.timezone.get_result_time()
-        self.result_time_lable = PySide6.QtWidgets.QLabel(str(result_time))
-        result_timezone = self.timezone.get_result_timezone()
-        self.result_timezone_lable = PySide6.QtWidgets.QLabel(str(result_timezone))
+        self.result_time = self.timezone.get_result_time()
+        self.result_time_lable = PySide6.QtWidgets.QLabel(str(self.result_time))
+        self.result_timezone = self.timezone.get_result_timezone()
+        self.result_timezone_lable = PySide6.QtWidgets.QLabel(str(self.result_timezone))
 
         result_timeLayout.addWidget(self.result_time_lable)
         result_timeLayout.addWidget(self.result_timezone_lable)  
@@ -47,19 +50,24 @@ class MainWindow(PySide6.QtWidgets.QMainWindow):
         widegt = PySide6.QtWidgets.QWidget()
         widegt.setLayout(mainLayout)
         self.setCentralWidget(widegt)
+        
 
         self.timer = PySide6.QtCore.QTimer(self)
         self.timer.timeout.connect(self.update_time)
         self.timer.start(1000)
 
-        self.setBaseSize(500,500)
+        self.resize(500,300)
 
     def update_time(self):
         current_time = self.timezone.get_current_time()
         result_time = self.timezone.get_result_time()
         self.time_lable.setText(str(current_time))
         self.result_time_lable.setText(str(result_time))
-    
+
+    def change_result_time(self):
+        self.result_timezone = self.box.currentText()
+        self.result_timezone_lable.setText(self.result_timezone)
+        self.timezone.change_result_timezone(self.result_timezone)
 
 
 if __name__ == '__main__':
